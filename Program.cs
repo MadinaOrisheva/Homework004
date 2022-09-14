d8af81edb31ea8596afe104d8058b9563c5225e6
Madina <madina.orish@mail.ru>
2022-09-14 14:28:31 +0300

    1 zadacha

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+void Function(int a, int b) {
+int result = a;
+   
+ for (int curr = 1; curr<=b; result= result*a) {
+ curr++;
+ }
+
+Console.WriteLine ($"Число {a} в степени {b} равно {result}");
+
+    
+}
+ Console.WriteLine("Введите первое число: ");
+ int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Function (x,y);
+
